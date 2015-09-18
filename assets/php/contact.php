1362f6509af8f1ac85563c79ee66cb39022fab60
Regis Leray <regis.leray@appdirect.com>
2015-09-17 23:16:17 -0400

    first release website

---
+++ assets/php/contact.php
@@ -0,0 +1,63 @@
+<?php
+// your secret key
+$secret = "6LfZDw0TAAAAAM73HeWJNSjC3DtuOwNcp_KLhRP7";
+$emailTo = "christine.charpentier.llb@gmail.com";
+
+verify_and_send();
+
+function verify_and_send(){
+	$params = array(
+		"email" => $_POST['email'], 
+		"message" => $_POST['message'], 
+		"to" => $GLOBALS['emailTo'],
+		"subject" => "Contact From ".$_POST['name']);
+
+	try{
+		valid_captcha($_POST["g-recaptcha-response"]);
+		valid_email($params);
+		send_email($params);
+		$data = array("type" => "success", "message" => "Email Successful");
+	
+	}catch(Exception $e){
+		error_log("error ".$e->getMessage(), 0);
+		header('HTTP/1.1 400 Bad Request');
+		$data = array("type" => "error", "message" => $e->getMessage());
+	}
+
+	header('Content-Type: application/json; charset=UTF-8');
+	echo json_encode($data);
+}
+
+function send_email($params){
+	$headers = 'From: '. $params["email"] . "\r\n" .
+    'Reply-To: '. $params["email"] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($params["to"], $params["subject"], $params["message"], $headers);
+	if($status != TRUE){
+		throw new Exception("Cannot send the email");
+	}
+}
+
+function valid_email($params){
+	foreach ($params as $key => $value){
+		if( empty($value) ){
+			throw new InvalidArgumentException("Param " . $key . " is empty");
+		}
+	}
+}
+
+function valid_captcha($reCaptchaResponse) {
+	if( !isset($reCaptchaResponse) ){
+		throw new InvalidArgumentException("Captcha response is empty");
+	}
+	// empty response
+	$response = null;
+	// check secret key
+ 	$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$GLOBALS['secret']."&response=".$reCaptchaResponse."&remoteip=".$_SERVER['REMOTE_ADDR']);
+	
+	if($response == null && $response->success == false){
+		throw new Exception("Captcha Failed");
+	}
+}
+?>
