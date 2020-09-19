2b4331be1a7f98754e6afdca888347b330836ae7
regis-leray <regis.leray@gmail.com>
2020-09-19 15:26:32 -0400

    update website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -119,60 +119,6 @@
 		wow.init();
 
 
-		/* ---------------------------------------------- /*
-		 * E-mail validation
-		/* ---------------------------------------------- */
-
-		function isValidEmailAddress(emailAddress) {
-			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-			return pattern.test(emailAddress);
-		};
-
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
-
-		$('#contact-form').submit(function(e) {
-
-			e.preventDefault();
-
-			var c_name = $('#c_name').val();
-			var c_email = $('#c_email').val();
-			var c_message = $('#c_message').val();
-			var g_recaptcha_response = $('#g-recaptcha-response').val();
-			var response = $('#contact-form .ajax-response');
-			
-			response.find(".alert").hide();
-
-			var formData = {
-				'name'       : c_name,
-				'email'      : c_email,
-				'message'    : c_message,
-				'g-recaptcha-response' : g_recaptcha_response
-			};
-
-			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
-				response.find('.alert-danger').fadeIn(500).find(".message").html('Merci de corriger les erreurs et ressayer.');
-			}
-
-			else {
-					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'assets/php/contact.php', // the url where we want to POST
-							data        : formData, // our data object
-							dataType    : 'json', // what type of data do we expect back from the server
-							encode      : true
-						}).done(function( data, textStatus, jqXHR ) {
-							$('#contact-form .ajax-hidden').hide();
-							response.find(".alert-success").fadeIn(500).find(".message").html('Votre email a ete envoye');
-  							
-						}).fail(function(jqXHR, textStatus) {					
-							response.find(".alert-danger").fadeIn(500).find(".message").html(jqXHR.responseJSON.message);  						
-						});
-				}           
-            	return false;
-			});
-
 	});
 
 })(jQuery);
